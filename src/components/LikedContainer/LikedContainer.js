815c316f672976431ce198d877303d9d1ae3d810
KamajiiSC <brandonlschaen@gmail.com>
2020-11-06 17:58:38 -0500

    Created h2 title, container for like gifs, button to calculate, and a paragraph in case you need more liked gifs

--- src/components/LikedContainer/LikedContainer.js
+++ src/components/LikedContainer/LikedContainer.js
@@ -3,7 +3,12 @@ import './LikedContainer.css';
 
 const LikedContainer = () => (
   <div className="liked-container">
+    <h2>YOUR LIKED GIFS</h2>
+    <div className="liked-container">
 
+    </div>
+    <button>CALCULATE MY WEIRDNESS SCORE</button>
+    <p>You must like x more GIFs to calculate your score</p>
   </div>
 );  
 
