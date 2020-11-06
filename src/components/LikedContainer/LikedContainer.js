11f3402be897aa7788ad70a812170eec5bea96a3
KamajiiSC <brandonlschaen@gmail.com>
2020-11-06 15:48:01 -0500

    Created component LikedContainer and imported to App.js

---
+++ src/components/LikedContainer/LikedContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './LikedContainer.css';
+
+const LikedContainer = () => (
+  <div className="liked-container">
+
+  </div>
+);  
+
+export default LikedContainer;
