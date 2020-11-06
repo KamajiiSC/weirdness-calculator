f143e9e0cd6d171f30019882a856177cb7e9d965
KamajiiSC <brandonlschaen@gmail.com>
2020-11-06 16:47:45 -0500

    Corrected SelectionContainer import, Removed SearchContainer and ResultContainer from App and moved them to SelectionContainer for layout purposes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
 import Header from './components/Header/Header';
-import SearchContainer from './components/SearchContainer/SearchContainer';
-import ResultContainer from './components/ResultContainer/ResultContainer';
 import LikedContainer from './components/LikedContainer/LikedContainer';
-import Selectioncontainer from './components/SelectionContainer/SelectionContainer';
+import SelectionContainer from './components/SelectionContainer/SelectionContainer';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchContainer />
-      <ResultContainer />
+      <SelectionContainer />
       <LikedContainer />
     </div>
   );
--- src/components/SelectionContainer/SelectionContainer.js
+++ src/components/SelectionContainer/SelectionContainer.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './SelectionContainer';
+import SearchContainer from '../SearchContainer/SearchContainer';
+import ResultContainer from '../ResultContainer/ResultContainer';
 
 const SelectionContainer = () => (
   <div className="selection-container">
-
+    <SearchContainer />
+    <ResultContainer />
   </div>
 )
 
