8581c9b3d5b9a62c240445702f24160bb1010e1a
KamajiiSC <brandonlschaen@gmail.com>
2020-11-06 15:41:33 -0500

    Created component SearchContainer and imported to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import SearchContainer from './components/SearchContainer/SearchContainer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchContainer />
     </div>
   );
 }
---
+++ src/components/SearchContainer/SearchContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SearchContainer.css';
+
+const SearchContainer = () => (
+  <div className="search-container">
+
+  </div>
+);
+
+export default SearchContainer;
