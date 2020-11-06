dc2fdb901b8f49c10f3ce635bd552d1f79cfdf2d
KamajiiSC <brandonlschaen@gmail.com>
2020-11-06 16:00:08 -0500

    Created two paragraphs, desc-section-one and desc-section-two, wrapped inside a section tag with class description

--- src/components/SearchContainer/SearchContainer.js
+++ src/components/SearchContainer/SearchContainer.js
@@ -4,6 +4,15 @@ import './SearchContainer.css';
 const SearchContainer = () => (
   <div className="search-container">
 
+    <section className="description">
+      <p className="desc-section-one">
+        Find out how weird you are by selecting the GIFs that make you laugh. 
+        We'll show you the least weird ones to start, but you can move the slider to make them weirder.
+      </p>
+      <p className="desc-section-two">
+        When you find a GIF you like, press the Like button. Once you like 5 GIFs we'll show you how weird you are.
+      </p>
+    </section>
   </div>
 );
 
