4c3a1aec3569a1f65ff25e260b72572b89f33dbd
KamajiiSC <brandonlschaen@gmail.com>
2020-11-06 16:44:26 -0500

    Created SelectionContainer component and imported to App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import SearchContainer from './components/SearchContainer/SearchContainer';
 import ResultContainer from './components/ResultContainer/ResultContainer';
 import LikedContainer from './components/LikedContainer/LikedContainer';
+import Selectioncontainer from './components/SelectionContainer/SelectionContainer';
 
 function App() {
   return (
---
+++ src/components/SelectionContainer/SelectionContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SelectionContainer';
+
+const SelectionContainer = () => (
+  <div className="selection-container">
+
+  </div>
+)
+
+export default SelectionContainer;
