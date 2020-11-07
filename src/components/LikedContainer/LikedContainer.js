fd4ae0c800319975e9afc73368beaece8d82297b
KamajiiSC <brandonlschaen@gmail.com>
2020-11-07 13:49:01 -0500

    Added placeholder img to resources, imported and placed in 4 in liked gifs

--- src/components/LikedContainer/LikedContainer.js
+++ src/components/LikedContainer/LikedContainer.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './LikedContainer.css';
+import placeholder from '../../resources/placeholder-image.png';
 
 const LikedContainer = () => (
   <div className="liked-container">
     <h2>YOUR LIKED GIFS</h2>
     <div className="liked-list">
-
+      <img src={placeholder} alt="Place holder for selected GIFs"/>
+      <img src={placeholder} alt="Place holder for selected GIFs"/>
+      <img src={placeholder} alt="Place holder for selected GIFs"/>
+      <img src={placeholder} alt="Place holder for selected GIFs"/>
     </div>
     <div className="calc-btn-desc">
       <button>CALCULATE MY WEIRDNESS SCORE</button>
