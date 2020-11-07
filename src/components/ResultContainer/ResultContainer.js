f073a348dae6350b4e3b893aa68380f9feba8f60
KamajiiSC <brandonlschaen@gmail.com>
2020-11-07 14:07:13 -0500

    imported and added placeholder to result container

--- src/components/ResultContainer/ResultContainer.js
+++ src/components/ResultContainer/ResultContainer.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import './ResultContainer';
+import './ResultContainer.css';
+import placeholder from '../../resources/placeholder-image.png';
 
 const ResultContainer = () => (
   <div className="result-container">
     <h2>YOUR RESULT</h2>
     <div className="result-display">
       <p className="gif-name">example</p>
-      <img src="" alt="" className="gif-display"/>
+      <img src={placeholder} alt="Placeholder for GIF results" className="gif-display"/>
       <button type="submit">Like</button>
     </div>
   </div>
