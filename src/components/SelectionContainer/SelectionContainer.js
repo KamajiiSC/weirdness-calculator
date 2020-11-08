8ebfc9f362b85a878c47198d629267057d3409b2
KamajiiSC <brandonlschaen@gmail.com>
2020-11-08 18:15:05 -0500

    Comments

--- src/components/SelectionContainer/SelectionContainer.js
+++ src/components/SelectionContainer/SelectionContainer.js
@@ -9,11 +9,14 @@ export default class SelectionContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      // Url to fetch request from API
       gifUrl:"https://api.giphy.com/v1/gifs/translate?api_key=22yty9FIPGbEixUWW3U9UpR9QzOx1iCK&s=Hello",
+      // Url to embed into page of GIF
       embedUrl: ""
     };
   }
 
+  // Function sends fetch request to gifUrl then sets embedUrl in state to returned embed url
   getGif = () => {
     fetch(this.state.gifUrl)
       .then(response => response.json())
