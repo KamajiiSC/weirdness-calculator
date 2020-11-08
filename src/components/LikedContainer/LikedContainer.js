c579784b3e6934cdc45191305df8459a5a119b95
KamajiiSC <brandonlschaen@gmail.com>
2020-11-08 14:15:56 -0500

    Wrapped liked-container in a container for layout purposes. Liked-container now has width and such similar to search and result container

--- src/components/LikedContainer/LikedContainer.js
+++ src/components/LikedContainer/LikedContainer.js
@@ -3,17 +3,19 @@ import './LikedContainer.css';
 import placeholder from '../../resources/placeholder-image.png';
 
 const LikedContainer = () => (
-  <div className="liked-container">
-    <h2>YOUR LIKED GIFS</h2>
-    <div className="liked-list">
-      <img src={placeholder} alt="Place holder for selected GIFs"/>
-      <img src={placeholder} alt="Place holder for selected GIFs"/>
-      <img src={placeholder} alt="Place holder for selected GIFs"/>
-      <img src={placeholder} alt="Place holder for selected GIFs"/>
-    </div>
-    <div className="calc-btn-desc">
-      <button>CALCULATE MY WEIRDNESS SCORE</button>
-      <p>You must like x more GIFs to calculate your score</p>
+  <div className="liked-layout-container">
+    <div className="liked-container">
+      <h2>YOUR LIKED GIFS</h2>
+      <div className="liked-list">
+        <img src={placeholder} alt="Place holder for selected GIFs"/>
+        <img src={placeholder} alt="Place holder for selected GIFs"/>
+        <img src={placeholder} alt="Place holder for selected GIFs"/>
+        <img src={placeholder} alt="Place holder for selected GIFs"/>
+      </div>
+      <div className="calc-btn-desc">
+        <button>CALCULATE MY WEIRDNESS SCORE</button>
+        <p>You must like x more GIFs to calculate your score</p>
+      </div>
     </div>
   </div>
 );  
