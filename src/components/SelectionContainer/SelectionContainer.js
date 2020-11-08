595405ed193f2f55b9923262506ace0dd0a47b0c
KamajiiSC <brandonlschaen@gmail.com>
2020-11-08 15:42:05 -0500

    Converted SelectionContainer to class based component and setup state

--- src/components/SelectionContainer/SelectionContainer.js
+++ src/components/SelectionContainer/SelectionContainer.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SelectionContainer.css';
 import SearchContainer from '../SearchContainer/SearchContainer';
 import ResultContainer from '../ResultContainer/ResultContainer';
 
-const SelectionContainer = () => (
-  <div className="selection-container">
-    <SearchContainer />
-    <hr className="selection-divider" />
-    <ResultContainer />
-  </div>
-)
 
-export default SelectionContainer;
+
+export default class SelectionContainer extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render(){
+    return(
+      <div className="selection-container">
+        <SearchContainer />
+        <hr className="selection-divider" />
+        <ResultContainer />
+      </div>
+    )
+  }
+}
