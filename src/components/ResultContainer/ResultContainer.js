f53932c63196d95a3bf835d48011252c551a6893
KamajiiSC <brandonlschaen@gmail.com>
2020-11-08 14:52:41 -0500

    Added max, min, and step, and marks to slider

--- src/components/ResultContainer/ResultContainer.js
+++ src/components/ResultContainer/ResultContainer.js
@@ -4,6 +4,20 @@ import placeholder from '../../resources/placeholder-image.png';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
+const marks = {
+  0: 0,
+  1: 1,
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  10: 10
+}
+
 const ResultContainer = () => (
   <div className="result-container">
     <h2>YOUR RESULT</h2>
@@ -12,7 +26,7 @@ const ResultContainer = () => (
       <img src={placeholder} alt="Placeholder for GIF results" className="gif-display"/>
       <button type="submit">Like</button>
     </div>
-    <Slider />
+    <Slider min={0} max={10} step={1} marks={marks}/>
   </div>
 );
 
