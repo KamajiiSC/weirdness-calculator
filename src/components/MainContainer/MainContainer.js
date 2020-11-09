3daf3ff98263260321fa5443eeb48ffcc66490b9
KamajiiSC <brandonlschaen@gmail.com>
2020-11-09 17:02:55 -0500

    Added likes to state in MainContinaer

--- src/components/MainContainer/MainContainer.js
+++ src/components/MainContainer/MainContainer.js
@@ -4,7 +4,12 @@ import LikedContainer from '../LikedContainer/LikedContainer';
 import SelectionContainer from '../SelectionContainer/SelectionContainer';
 
 export default class MainContainer extends Component {
-  
+  constructor(props){
+    super(props);
+    this.state = {
+      likes: []
+    }
+  };
   
   render(){
     return(
