818742ae1dbbd8001c251f0a522c3c1d297fb15c
KamajiiSC <brandonlschaen@gmail.com>
2020-11-09 11:37:38 -0500

    Made getGif set returnGif to the object the fetch returns. Passed returnGif down to resultContainer. Set state on componentDidUpdate so embedUrl is set to the object for now to make sure it works

--- src/components/ResultContainer/ResultContainer.js
+++ src/components/ResultContainer/ResultContainer.js
@@ -26,6 +26,16 @@ export default class ResultContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      embedUrl: ''
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.returnGif !== prevProps.returnGif) {
+      console.log(this.props.returnGif)
+      this.setState({
+        embedUrl: this.props.returnGif
+      })
     }
   }
 
@@ -35,7 +45,7 @@ export default class ResultContainer extends Component {
         <h2>YOUR RESULT</h2>
         <div className="result-display">
           <p className="gif-name">example</p>
-          <img src={placeholder} alt="Placeholder for GIF results" className="gif-display"/>
+          <img src={this.state.embedUrl} alt="Placeholder for GIF results" className="gif-display"/>
           <button type="submit">Like</button>
         </div>
         <div className="slider-container">
--- src/components/SelectionContainer/SelectionContainer.js
+++ src/components/SelectionContainer/SelectionContainer.js
@@ -11,8 +11,9 @@ export default class SelectionContainer extends Component {
     this.state = {
       // Url to fetch request from API
       gifUrl:"https://api.giphy.com/v1/gifs/translate?api_key=22yty9FIPGbEixUWW3U9UpR9QzOx1iCK&s=Hello",
+
       // Url to embed into page of GIF
-      embedUrl: ""
+      returnGif: {}
     };
   }
 
@@ -21,9 +22,9 @@ export default class SelectionContainer extends Component {
     fetch(this.state.gifUrl)
       .then(response => response.json())
       .then(data => {
-        this.setState({ embedUrl: data.data.embed_url });
+        this.setState({ returnGif: data.data});
         })
-      .then(console.log(this.state.embedUrl))
+      .then(console.log("hell"))
   }
 
   render(){
@@ -31,7 +32,7 @@ export default class SelectionContainer extends Component {
       <div className="selection-container">
         <SearchContainer getGif={this.getGif} />
         <hr className="selection-divider" />
-        <ResultContainer />
+        <ResultContainer returnGif={this.state.returnGif}/>
       </div>
     )
   }
