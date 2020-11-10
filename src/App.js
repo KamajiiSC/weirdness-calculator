d6ef87c8fbeb4bceb42e5270fedb2d5a2d354a6d
KamajiiSC <brandonlschaen@gmail.com>
2020-11-10 18:59:48 -0500

    Added and imported react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header/Header';
 import MainContainer from './components/MainContainer/MainContainer';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
