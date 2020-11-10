d0f3977ec48235e8750696044d473cc83eca6d30
KamajiiSC <brandonlschaen@gmail.com>
2020-11-10 18:12:11 -0500

    Imported store to likedcontainer and set use effects to update gif urls in state with a try catch. Liked gifs now appear in liked container

--- src/components/LikedContainer/LikedContainer.js
+++ src/components/LikedContainer/LikedContainer.js
@@ -1,23 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './LikedContainer.css';
 import placeholder from '../../resources/placeholder-image.png';
+import { useSelector } from 'react-redux';
 
-const LikedContainer = () => (
-  <div className="liked-layout-container">
-    <div className="liked-container">
-      <h2>YOUR LIKED GIFS</h2>
-      <div className="liked-list">
-        <img src={placeholder} alt="Place holder for selected GIFs"/>
-        <img src={placeholder} alt="Place holder for selected GIFs"/>
-        <img src={placeholder} alt="Place holder for selected GIFs"/>
-        <img src={placeholder} alt="Place holder for selected GIFs"/>
-      </div>
-      <div className="calc-btn-desc">
-        <button>CALCULATE MY WEIRDNESS SCORE</button>
-        <p>You must like x more GIFs to calculate your score</p>
+const LikedContainer = () => {
+  const likes = useSelector(state => state.likedGifs);
+  const [firstGif, setFirst] = useState(likes[0]);
+  const [secondGif, setSecond] = useState(likes[1]);
+  const [thirdGif, setThird] = useState(likes[2]);
+  const [fourthGif, setFourth] = useState(likes[3]);
+
+  useEffect(() => {
+    try{setFirst(likes[0].url)}
+    catch(err) {
+      setFirst(likes[0])
+    }
+    console.log(likes[0])
+  }, [useSelector(state => state.likedGifs)])
+
+  useEffect(() => {
+    try{setSecond(likes[1].url)}
+    catch(err) {
+      setSecond(likes[1])
+    }
+    console.log(likes[1])
+  }, [useSelector(state => state.likedGifs)])
+
+  useEffect(() => {
+    try{setThird(likes[2].url)}
+    catch(err) {
+      setThird(likes[2])
+    }
+    console.log(likes[2])
+  }, [useSelector(state => state.likedGifs)])
+
+  useEffect(() => {
+    try{setFourth(likes[3].url)}
+    catch(err) {
+      setFourth(likes[3])
+    }
+    console.log(likes[3])
+  }, [useSelector(state => state.likedGifs)])
+
+  return(
+    <div className="liked-layout-container">
+      <div className="liked-container">
+        <h2>YOUR LIKED GIFS</h2>
+        <div className="liked-list">
+          <img src={firstGif || placeholder} alt="Place holder for selected GIFs"/>
+          <img src={secondGif || placeholder} alt="Place holder for selected GIFs"/>
+          <img src={thirdGif || placeholder} alt="Place holder for selected GIFs"/>
+          <img src={fourthGif || placeholder} alt="Place holder for selected GIFs"/>
+        </div>
+        <div className="calc-btn-desc">
+          <button>CALCULATE MY WEIRDNESS SCORE</button>
+          <p>You must like x more GIFs to calculate your score</p>
+        </div>
       </div>
     </div>
-  </div>
-);  
+  )
+};  
 
 export default LikedContainer;
