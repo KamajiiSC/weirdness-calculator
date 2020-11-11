2c0f2beacd5e51f4c12edeefb7d8d30d1d6e79db
KamajiiSC <brandonlschaen@gmail.com>
2020-11-10 19:04:50 -0500

    Created ResultsPage and imported to App. In App created Router with Switch and Routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import Header from './components/Header/Header';
 import MainContainer from './components/MainContainer/MainContainer';
+import ResultsPage from './components/ResultsPage/ResultsPage';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MainContainer />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <MainContainer />
+          </Route>
+          
+          <Route path="/results">
+
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
