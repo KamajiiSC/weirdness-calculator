74fac199cd2d60a4664b3b9b60d656007cee7192
KamajiiSC <brandonlschaen@gmail.com>
2020-11-10 19:19:29 -0500

    Setup state in results page and changed results likedlistresults name

--- src/components/ResultsPage/ResultsPage.js
+++ src/components/ResultsPage/ResultsPage.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ResultsPage.css';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const ResultsPage = () => {
+  const likes = useSelector(state => state.likedGifs);
+  const [firstGif, setFirst] = useState(likes[0]);
+  const [secondGif, setSecond] = useState(likes[1]);
+  const [thirdGif, setThird] = useState(likes[2]);
+  const [fourthGif, setFourth] = useState(likes[3]);
 
   return(
     <div>
       <h2>You scored an out of 10 on the weirdness scale!</h2>
       <h4>The GIFs you liked</h4>
-      <div className="liked-list">
+      <div className="liked-list-results">
           <img src="" alt="Place holder for selected GIFs"/>
           <img src="" alt="Place holder for selected GIFs"/>
           <img src="" alt="Place holder for selected GIFs"/>
