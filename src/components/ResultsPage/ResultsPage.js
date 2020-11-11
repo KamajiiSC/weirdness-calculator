a67853048de067dcede184a8c42fe3e13a1d23d5
KamajiiSC <brandonlschaen@gmail.com>
2020-11-10 19:13:28 -0500

    Setup elements for results page

--- src/components/ResultsPage/ResultsPage.js
+++ src/components/ResultsPage/ResultsPage.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './ResultsPage.css';
+import { Link } from 'react-router-dom';
 
 const ResultsPage = () => {
 
   return(
     <div>
-      
+      <h2>You scored an out of 10 on the weirdness scale!</h2>
+      <h4>The GIFs you liked</h4>
+      <div className="liked-list">
+          <img src="" alt="Place holder for selected GIFs"/>
+          <img src="" alt="Place holder for selected GIFs"/>
+          <img src="" alt="Place holder for selected GIFs"/>
+          <img src="" alt="Place holder for selected GIFs"/>
+      </div>
+      <Link><button>START OVER</button></Link>
     </div>
   )
 };
